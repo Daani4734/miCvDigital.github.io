cfa2bd0111a810c968af8bef9621ae432f6c9d8a
Daniel matias <96064702+Daani4734@users.noreply.github.com>
2023-03-08 19:38:23 -0300

    misArchivosDigitales

---
+++ java/funciones.js
@@ -0,0 +1,16 @@
+/*var callUrl = {
+    call: function(){
+        jQuery("#navegacion . nav-link").click();
+    };
+    miPerfilBtn : function(e) {
+        e.preventDefault();
+        alert("hello word");
+    },
+    precarga:function(){
+
+    },
+};
+
+//jQuery(document).ready(callUrl.call)
+//jQuery(windows).on("load",acciones.precarga)
+*/
---
+++ java/main.js
@@ -0,0 +1,122 @@
+
+
+let miPerfil = document.getElementById('miLinkPerfil');
+let miVercel = document.getElementById('miWebSide');
+let contacto = document.getElementById('miLinkContacto');
+let enlace = "/img/imgCurricula.gif";
+
+
+
+
+
+
+
+// miWeb.addEventListener('click' () =>{
+ //   miVercel.promp(vercel.com);
+ //};
+
+
+
+
+
+//const linkVerecel = URL("https://curriculum-vitae-4-0-0-1.vercel.app/");
+
+
+/*
+toggle.addEventListener('click', function(){
+    this.classList.toggle('logoWeb');
+     if(this.classList.toogle('navbar-brand')){
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+     }
+    });
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//funciones de clik en enlaces
+//crear una fucion de evento que ejecute cada vez que haga, 
+//ue al hacer clik cada <a> me lleve a un enlace</a> enlace....
+/*
+const goLink = () => {
+    location.href="enlace";
+
+}*/
+
+/* esta es la funcion que quiero pero como hago que cada una se ejecute por separado
+let perfil = () => {
+    miPerfil.addEventListener('click', enlace);
+    return;
+    miPortafolio.addEventListener('click', portafolio);
+};
+
+
+
+
+
+
+let redes = () => {
+
+}
+
+
+//function para cambiar background-image
+/*
+let changeImg = (sw) =>{
+    var change;
+    if (sw == 0) {
+        change = "imagen.jpg"
+    } else{
+        change = "segundaImagen.jpg"
+    }
+document.getElementById('fondo');
+};
+*/
+
+
+
+
+
+
+
+
+/*
+const misLink = () =>{
+    miPerfil.addEventListener('clik', listaPieDePag).location.;
+}
+
+const redes = () => {
+    
+}*/
+//let clikNav = document.addEventListener('clik', miPerfil);
+//    window.location.href = 
+
+
+//bucle para ver cantidad de selectores, cases, id, container
+ /*   for (var i = 0;i < id.length; i++) {
+        id[i].id = "enlaces"+i;
+
+    }*/
+
+//cambiar imagenes de la seccion mis experiencias
+
+
